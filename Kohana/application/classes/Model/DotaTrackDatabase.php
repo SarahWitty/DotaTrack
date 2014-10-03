ab2c7c43f246f512d80a5d090c0ea5972099cf37
SarahWitty <sarahewitty@gmail.com>
2014-10-03 09:49:06 -0500

    Add ORM to DotaTracksDatabase

--- Kohana/application/classes/Model/DotaTrackDatabase.php
+++ Kohana/application/classes/Model/DotaTrackDatabase.php
@@ -1,10 +1,42 @@
-class Model_DotaTrackDatabase extends Model_Database
+class Model_DotaTrackDatabase extends ORM
 {
-	public function do_stuff()
-	{
-	}
-	
-	public function get_stuff()
-	{
+	Database::$default = 'dotatrack';
+
+	public function rules(){
+		return array(
+			'matchId' => array(
+				array('not_empty'),
+				array('min_length', array(':value', 1)),
+				array('max_length', array(':value', 10)),
+			),
+			'skillLevel' => array(
+				array('not_empty'),
+				array('length', array(':value', 1)),
+			),
+			'duration' => array(
+				array('not_empty'),
+				array('min_length', array(':value', 1)),
+			),
+			'result' => array(
+				array('not_empty'),
+				array('length', array(':value', 1)),
+			),
+			'gameMode' => array(
+				array('not_empty'),
+				array('min_length', array(':value', 1)),
+				array('max_length', array(':value', 2)),
+			),
+			'region' => array(
+				array('not_empty'),
+				array('length', array(':value', 3)),
+			),
+			'date' => array(
+				array('not_empty'),
+			),
+			'matchType' => array(
+				array('not_empty'),
+				array('length', array(':value', 1)),
+			),			
+		);
 	}
 }
