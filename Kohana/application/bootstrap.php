9833acee1032c2fb94652c7141d0c7d9d36e164a
Hazen Johnson <leyarotheconquerer@yahoo.com>
2014-10-07 20:05:12 -0500

    Change default controller to the testing controller.

--- Kohana/application/bootstrap.php
+++ Kohana/application/bootstrap.php
@@ -138,6 +138,6 @@ Kohana::modules(array(
  */
 Route::set('default', '(<controller>(/<action>(/<id>)))')
 	->defaults(array(
-		'controller' => 'login',
+		'controller' => 'testing',
 		'action'     => 'index',
 	));
