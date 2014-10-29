ec1cfc28ca691ec52e8a41b13c1ce2608f5e3ef7
Summit41 <summit@dorm41.org>
2014-10-28 21:56:56 -0500

    Added basic login page, login.css, the dotatrack logo, the resources folder, and some temporary login code

--- Kohana/application/classes/Controller/Login.php
+++ Kohana/application/classes/Controller/Login.php
@@ -11,4 +11,30 @@ class Controller_Login extends Controller {
 
 		$this->response->body($generated_view);
 	}
+	
+	public function action_potato()
+	{
+		$view = View::Factory('login2');
+		
+		if ($this->request->post('pid')) {
+			$view->has_pid = true;
+			$view->pid = $this->request->post('pid');
+			$view->output = Model::factory('Api')->get_match_history(array(array("playerId","=",$view->pid),array("matchId",">","973740011")));
+		}
+		else {
+			$view->has_pid = false;
+			$view->output = "Please log in first.";
+		}
+		
+		
+		$view->request = $this->request->post('pid');
+		
+		$generated_view = $view->render();
+
+		$stuff = Debug::dump($generated_view);
+
+		$this->response->body($generated_view);
+		
+	}
 }
+?>
--- Kohana/application/views/login.php
+++ Kohana/application/views/login.php
@@ -5,5 +5,8 @@
 	<body>
 		<h1>Login Page</h1>
 		<p>Normally, you'd be able to log in from here.</p>
+		
+
+		/Login/index
 	</body>
 </html>
