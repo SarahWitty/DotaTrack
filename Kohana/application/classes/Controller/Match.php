750f6d5bb263dce81c4b1579b2b0215175dee6a5
SarahWitty <sarahewitty@gmail.com>
2014-10-31 09:41:36 -0500

    Made Match.php handle null value.

--- Kohana/application/classes/Controller/Match.php
+++ Kohana/application/classes/Controller/Match.php
@@ -9,14 +9,22 @@ class Controller_Match extends Controller_DotaTrack {
 
 		$matchId = $this->request->param('matchId');
 		$model = Model::Factory('DotaTrack');
+		
 		$result = $model->get_match_data($matchId);
-		//die(Debug::vars($result));
+		
+		if(!$result){
+			$view->performance = array(array());
+			$view->match = array();
+		
+		}else{
+			//die(Debug::vars($result));
 
-		$view->output = $result;
-		$view->performance = $result['playerPerformance'];
+			$view->output = $result;
+			$view->performance = $result['playerPerformance'];
 
-		unset($result['playerPerformance']);
-		$view->match = $result;
+			unset($result['playerPerformance']);
+			$view->match = $result;
+		}
 
 		$this->add_header();
 		$this->add_view_content($view->render());
--- Kohana/application/views/match/index.php
+++ Kohana/application/views/match/index.php
@@ -1,7 +1,6 @@
 <link rel="stylesheet" type="text/class" href="<?php echo URL::base(); ?>resources/styles/match.css" />
 <h1>Match page</h1>
 <p>Normally, this would tell you useful information about a match.</p>
-<!-- <pre><?php echo Debug::dump($output)?></pre> -->
 <table class="matchTable">
 	<tr>
 	<?php
