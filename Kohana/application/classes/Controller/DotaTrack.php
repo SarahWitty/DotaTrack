33e199b768d26d366d283b9845ef7bb57cb70ab3
Hazen Johnson <leyarotheconquerer@yahoo.com>
2014-10-31 08:59:56 -0500

    Add nicify function for match data.

--- Kohana/application/classes/Controller/DotaTrack.php
+++ Kohana/application/classes/Controller/DotaTrack.php
@@ -22,6 +22,21 @@ class Controller_DotaTrack extends Controller_Template {
 			$this->template->body = $header;
 		}
 	}
+
+	/**
+	 * Takes the id's present in the match data and translates it into human
+	 * readable junk.
+	 *
+	 * @param $matchData Contains a whole bunch of relatively confusing ids for
+	 * the match data. The data structure is equivalent to what get_match_data
+	 * will return for most models.
+	 *
+	 * @return A revised $matchData containing human readable strings.
+	 */
+	protected function nicify_match_data($matchData)
+	{
+		return $matchData;
+	}
 }
 
 ?>
