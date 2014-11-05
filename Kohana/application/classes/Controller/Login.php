8731b66472606fdee51d14ab0da9bb0d2922976b
Summit41 <summit@dorm41.org>
2014-11-04 20:31:16 -0600

    Started Session in Login controller Set userId in Session Cleaned up Login controller: 	No longer uses /Login/potato 	No longer uses view/login/login2 	Removed previous test output

--- Kohana/application/classes/Controller/Login.php
+++ Kohana/application/classes/Controller/Login.php
@@ -5,29 +5,19 @@ class Controller_Login extends Controller_DotaTrack {
 	public function action_index()
 	{
 		$view = View::Factory('login/login');
-
-		$generated_view = $view->render();
-
-		$stuff = Debug::dump($generated_view);
-
-		$this->add_view_content($generated_view);
-	}
-
-	public function action_potato()
-	{
-		$view = View::Factory('login/login2');
-
+		
+		session_start();
+		
 		if ($this->request->post('pid')) {
 			$view->has_pid = true;
-			$view->pid = $this->request->post('pid');
-			$view->output = Model::factory('Api')->get_match_history(array(array("playerId","=",$view->pid),array("matchId",">","907491716")));
+			$_SESSION["userId"] = $this->request->post('pid');
+			$view->output = "Redirecting...";
 		}
 		else {
 			$view->has_pid = false;
 			$view->output = "Please log in first.";
 		}
-
-
+		
 		$view->request = $this->request->post('pid');
 
 		$generated_view = $view->render();
@@ -35,7 +25,6 @@ class Controller_Login extends Controller_DotaTrack {
 		$stuff = Debug::dump($generated_view);
 
 		$this->add_view_content($generated_view);
-
 	}
 }
 ?>
--- Kohana/application/views/login/login.php
+++ Kohana/application/views/login/login.php
@@ -1,2 +1,15 @@
-<h1>Login Page</h1>
-<p>Normally, you'd be able to log in from here.</p>
+<link rel='stylesheet' href="<?php echo URL::base() ?>resources/styles/login.css">
+<div id='appTitle'>
+	<p>DOTA<br />TRACK</p>
+</div>
+<div id='login'>
+	<form method='POST'>
+		<p>PlayerId: <input type='text' name='pid'/><br /></p>
+		<input type='submit' value="Login" name='submit' />
+	<?php if ($has_pid):?>
+		<p>Welcome, <?=$pid ?></p>
+		<p><?php echo Debug::dump($output) ?></p>
+	<?php endif;?>
+	<p><?php  ?></p>
+	</form>
+</div>
