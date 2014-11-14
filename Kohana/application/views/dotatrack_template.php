5d427b344bc4996cd22daf1566e02271cf7d8a69
Hazen Johnson <leyarotheconquerer@yahoo.com>
2014-11-13 23:33:30 -0600

    Add NVD3 graphing library.

--- Kohana/application/views/dotatrack_template.php
+++ Kohana/application/views/dotatrack_template.php
@@ -2,8 +2,11 @@
 	<head>
 		<title>Dota Track</title>
 		<link rel='stylesheet' href="<?php echo URL::base() ?>resources/styles/dotatrack.css">
+		<link rel='stylesheet' href="<?php echo URL::base() ?>resources/styles/nv.d3.css">
 		<link rel="shortcut icon" href="<?php echo URL::base() ?>resources/BrowserTabIcon.png?v=2">
 		<script type="text/javascript" src="<?php echo URL::base() ?>resources/scripts/jquery-2.1.1.js"></script>
+		<script type="text/javascript" src="<?php echo URL::base() ?>resources/scripts/d3.v3.js"></script>
+		<script type="text/javascript" src="<?php echo URL::base() ?>resources/scripts/nv.d3.js"></script>
 		<script type="text/javascript">
 			<?=$javascript?>
 		</script>
