fa2cfe6106937936b05fa5cf983023011245a098
SarahWitty <sarahewitty@gmail.com>
2014-11-21 09:39:17 -0600

    dynamicaly set the max y value in the statistic page graphs.

--- Kohana/application/views/statistics/index.php
+++ Kohana/application/views/statistics/index.php
@@ -1,29 +1,29 @@
 <script type='text/javascript' src='<?php echo URL::base(); ?>resources/scripts/statistics.js'></script>
 <div class="graphGroup">
 	<h2>Kills</h2>
-	<svg style="width:300px;" id="kills"></svg>
+	<svg class="graph"  id="kills"></svg>
 </div>
 <div class="graphGroup">
 	<h2>Deaths</h2>
-	<svg style="width:300px;" id="deaths"></svg>
+	<svg class="graph" id="deaths"></svg>
 </div>
 <div class="graphGroup">
 	<h2>Assists</h2>
-	<svg style="width:300px;" id="assists"></svg>
+	<svg sclass="graph" id="assists"></svg>
 </div>
 <div class="graphGroup">
 	<h2>Last Hits</h2>
-	<svg style="width:300px;" id="lastHits"></svg>
+	<svg class="graph" id="lastHits"></svg>
 </div>
 <div class="graphGroup">
 	<h2>Denies</h2>
-	<svg style="width:300px;" id="denies"></svg>
+	<svg class="graph" id="denies"></svg>
 </div>
 <div class="graphGroup">
 	<h2>Experience per Minute</h2>
-	<svg style="width:300px;" id="xpm"></svg>
+	<svg class="graph" id="xpm"></svg>
 </div>
 <div class="graphGroup">
 	<h2>Gold per Minute</h2>
-	<svg style="width:300px;" id="gpm"></svg>
+	<svg class="graph" id="gpm"></svg>
 </div>
